ab7a92672688e54cbab1deffeca8b8ecb1031551
Sam Hall <mann.samantha11@gmail.com>
2020-12-10 17:27:47 -0500

    commit 2

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,29 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name_par, owner_par)
+    @name = name_par
+    @owner = owner_par
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
+
+# Cat
+#   Instance methods
+#     initializes with a name and an Owner (FAILED - 1)
+#     can change its owner (FAILED - 2)
+#     can't change its name (FAILED - 3)
+#     initializes with a nervous mood (FAILED - 4)
+#     can change its mood (FAILED - 5)
+#   Class methods
+#     knows all the cats (FAILED - 6)
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,41 @@
+# require 'pry'
 class Owner
   attr_reader :name, :species
+
+  @@all = []
+  @@owner_count = 0
+  
 
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self
+    @@owner_count += 1
   end
 
   def say_species
     "I am a #{@species}."
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@owner_count
+  end
+
+  def self.reset_all
+    @@all = []
+    @@owner_count = 0
+  end
+
 end
+
+# binding.pry
+# 0
+
+# Class methods
+#     .all returns all instances of Owner that have been created (FAILED - 1)
+#     .count returns the number of owners that have been created (FAILED - 2)
+#     .reset_all can reset the owners that have been created (FAILED - 3)
